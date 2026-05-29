beae95679883eb27776d304093b7a16588976a55
anky894 <anky894@proton.me>
2026-05-29 07:18:17 +0000

    avalon: Kang vibrator from pong
    Change-Id: I751b14ea8ed1cb0949431f976bddbcc3cebb437d

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 # ViPER4Android FX
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Vibrator
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/vendor.qti.hardware.vibrator.service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.vibrator.service.rc
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
