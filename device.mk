4fd6ed4c622b4729cd37b49069683fb63c63ab1f
EternalMikaelson <klaus_mikaelson@rediffmail.com>
2026-07-02 12:37:51 +0530

    Revert "avalon: Enable Oplus awinic support"
    This reverts commit 88250ca1b3200154bc512f1d6726d23b37faef22.

--- device.mk
+++ device.mk
@@ -60,7 +60,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.oplus-richtap
 
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_RICHTAP_EFFECT_REMAP,true)
-$(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USES_OPLUS_AWINIC,true)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
